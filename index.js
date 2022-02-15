62eff7c03a53bf2b2b7936be6248c84cbf1fb07b
Mahad410 <mahadqureshi410@gmail.com>
2022-02-14 19:42:07 -0600

    calculator

--- index.js
+++ index.js
@@ -1,103 +1,191 @@
 const display = document.getElementById('screen');
-// zero
-const zero = document.getElementById('zero').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText += '';
-    } else {
-        display.innerText += 0;
-    }
-});
-// one
-const one = document.getElementById('one').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +1;
-    } else {
-        display.innerText += 1;
-    }
-});
-// two
-const two = document.getElementById('two').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +2;
-    } else {
-        display.innerText += 2;
-    }
-});
-// three
-const three = document.getElementById('three').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +3;
-    } else {
-        display.innerText += 3;
-    }
-});
-// four
-const four = document.getElementById('four').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +4;
-    } else {
-        display.innerText += 4;
-    }
-});
-// five
-const five = document.getElementById('five').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +5;
-    } else {
-        display.innerText += 5;
-    }
-});
-// six
-const six = document.getElementById('six').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +6;
-    } else {
-        display.innerText += 6;
-    }
-});
+// const hDisplay = document.getElementById('h_screen');
+// let x = 0;
+// let y = 0;
+// var opr = '';
+// // zero
+// const zero = document.getElementById('zero').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText += '';
+//     } else {
+//         display.innerText += 0;
+//     }
+// });
+// // one
+// const one = document.getElementById('one').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +1;
+//     } else {
+//         display.innerText += 1;
+//     }
+// });
+// // two
+// const two = document.getElementById('two').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +2;
+//     } else {
+//         display.innerText += 2;
+//     }
+// });
+// // three
+// const three = document.getElementById('three').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +3;
+//     } else {
+//         display.innerText += 3;
+//     }
+// });
+// // four
+// const four = document.getElementById('four').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +4;
+//     } else {
+//         display.innerText += 4;
+//     }
+// });
+// // five
+// const five = document.getElementById('five').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +5;
+//     } else {
+//         display.innerText += 5;
+//     }
+// });
+// // six
+// const six = document.getElementById('six').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +6;
+//     } else {
+//         display.innerText += 6;
+//     }
+// });
 
-// seven
-const seven = document.getElementById('seven').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +7;
-    } else {
-        display.innerText += 7;
-    }
-});
+// // seven
+// const seven = document.getElementById('seven').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +7;
+//     } else {
+//         display.innerText += 7;
+//     }
+// });
 
-// eight
-const eight = document.getElementById('eight').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +8;
-    } else {
-        display.innerText += 8;
-    }
-});
+// // eight
+// const eight = document.getElementById('eight').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         display.innerText = +8;
+//     } else {
+//         display.innerText += 8;
+//     }
+// });
 
-// nine
-const nine = document.getElementById('nine').addEventListener('click', () => {
-    if (display.innerHTML === '0' || display.innerHTML === 0) {
-        display.innerText = +9;
-    } else {
-        display.innerText += 9;
-    }
-});
-// point
-const point = document.getElementById('point').addEventListener('click', () => {
-    if (display.innerHTML == '') {
-        display.innerText += '0.';
-    } else {
-        display.innerText += '.';
-    }
-});
+// // nine
+// const nine = document.getElementById('nine').addEventListener('click', () => {
+//     if (display.innerHTML === '0' || display.innerHTML === 0) {
+//         parseInt(display.innerText = +9);
+//     } else {
+//         parseInt(display.innerText += 9);
+//     }
+//     console.log(typeof(display.innerText))
+// });
+// // point
+// const point = document.getElementById('point').addEventListener('click', () => {
+//     if (display.innerHTML == '') {
+//         display.innerText += '0.';
+//     } else {
+//         display.innerText += '.';
+//     }
+// });
 
-// clear
-const clear = document.getElementById('C').addEventListener('click', () => {
+// // clear
+// const clear = document.getElementById('C').addEventListener('click', () => {
+//     display.innerText = '';
+//     hDisplay.innerText = '';
+// });
+
+// //backspace
+// const del = document.getElementById('del').addEventListener('click', () => {
+//     const value = display.innerText;
+//     display.innerText = display.innerText.slice(0, display.innerText.length - 1)
+// });
+
+// // +
+// const plus = document.getElementById('plus').addEventListener('click', () => {
+//     opr = '+';
+//     if (display.innerHTML === '') {
+//         display.value = x;
+//     } else {
+//         x = display.innerHTML;
+
+//     }
+//     parseInt(x);
+//     console.log(x, typeof(x))
+// });
+
+// // =
+// const equal = document.getElementById('result').addEventListener('click', () => {
+//     y = display.innerText;
+//     hDisplay.innerHTML += opr + y;
+// });
+
+var x = 0;
+var y = 0
+var opr = 0;
+var num = 0;
+var res = 0;
+
+function number(num) {
+    if (opr == 0) {
+        x = display.innerText += num;
+    } else if (opr != 0 && opr != '^2') {
+        y = display.innerText += num;
+    }
+    const plus = document.getElementById('plus').addEventListener('click', () => {
+        opr = '+';
+        display.innerText = '';
+    });
+
+    const minus = document.getElementById('minus').addEventListener('click', () => {
+        opr = '-';
+        display.innerText = '';
+    });
+
+    const multi = document.getElementById('multiply').addEventListener('click', () => {
+        opr = '*';
+        display.innerText = '';
+    });
+    const divide = document.getElementById('divide').addEventListener('click', () => {
+        opr = '/';
+        display.innerText = '';
+    });
+    const square = document.getElementById('x_x').addEventListener('click', () => {
+        opr = '^2';
+        display.innerText = "Press '='";
+    });
+    const equal = document.getElementById('result').addEventListener('click', () => {
+        if (opr == '+') {
+            res = parseInt(x) + parseInt(y);
+            display.innerText = res;
+        } else if (opr == '-') {
+            res = parseInt(x) - parseInt(y);
+            display.innerText = res;
+        } else if (opr == '*') {
+            res = parseInt(x) * parseInt(y);
+            display.innerText = res;
+        } else if (opr == '/') {
+            res = parseInt(x) / parseInt(y);
+            display.innerText = res;
+        } else if (opr == '^2') {
+            res = parseInt(x) * 2;
+            display.innerText = res;
+        }
+    });
+}
+
+function del() {
+    display.innerText = display.innerText.slice(0, display.innerText.length - 1);
+}
+
+function clr() {
     display.innerText = '';
-});
-
-//backspace
-const del = document.getElementById('del').addEventListener('click', () => {
-    const value = display.innerText;
-    display.innerText = display.innerText.slice(0, display.innerText.length - 1)
-});
+    console.log(typeof(display.innerText));
+}
